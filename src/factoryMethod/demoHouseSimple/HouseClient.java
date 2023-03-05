c57952be4e45fa7137843ee653560d2ef07cb73a
zenghailiang <zenghailiang@nowcoder.com>
2023-03-05 12:01:36 +0800

    优化逻辑增加try catch

--- src/factoryMethod/demoHouseSimple/HouseClient.java
+++ src/factoryMethod/demoHouseSimple/HouseClient.java
@@ -4,10 +4,14 @@ import factoryMethod.demoHouseSimple.entity.AbstractHouse;
 
 public class HouseClient {
     public static void main(String[] args) {
-        AbstractHouse whiteHouse = new HouseFactory().createHouse("白房子");
-        whiteHouse.show(32, 60, 1);
-        System.out.println("-------------------------------");
-        AbstractHouse redHouse = new HouseFactory().createHouse("红房子");
-        redHouse.show(122, 545, 35);
+        try {
+            AbstractHouse whiteHouse = new HouseFactory().createHouse("白房子");
+            whiteHouse.show(32, 60, 1);
+            System.out.println("-------------------------------");
+            AbstractHouse redHouse = new HouseFactory().createHouse("红房子");
+            redHouse.show(122, 545, 35);
+        } catch (Exception e) {
+            System.out.println("房子没找到");
+        }
     }
 }
